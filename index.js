a19f14b631dc243cfd3fb1a9a75daac665a3b793
Jiaaa1014 <linao264590000@gmail.com>
2018-02-24 17:58:11 +0800

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-
+var pg = require('pg');
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -15,9 +15,37 @@ app.get('/', function (request, response) {
 });
 
 app.get('/cool', function (request, response) {
-  response.send(cool());
+  response.send(cool.faces);
 });
 
+app.get('/times', function (request, response) {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (let i = 0; i < times; i++) result += i + ''
+
+  response.send(result)
+
+});
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+//     client.query('SELECT * FROM test_table', function (err, result) {
+//       done()
+//       if (err) console.log(err), response.send('Error', err)
+//       else response.render('pages/db', { result: result.rows })
+//     })
+//   })
+// })
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+    client.query('SELECT * FROM test_table', function (err, result) {
+      done();
+      if (err) { console.error(err); response.send("Error " + err); }
+      else { response.render('pages/db', { results: result.rows }); }
+    });
+  });
+});
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
